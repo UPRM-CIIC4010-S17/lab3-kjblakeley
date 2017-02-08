16d6c1c42d034e9dc041697f75ee8d1075f4a85b
3010a3 <3010a3@icaco.ece.uprm.edu>
2017-02-08 16:04:26 -0400

    title, size, location, default close operation have all been modified

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Kevin J Blakeley");
+		JFrame myFrame = new JFrame("Kevin J Blakeley Ramirez");
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
